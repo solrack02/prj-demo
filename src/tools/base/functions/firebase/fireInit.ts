986beb99c099b6e7689a7b53bde939447716beee
Carlos A. <carlosvsk@gmail.com>
2024-09-02 11:22:30 -0300

    Commit generated by Flaxboll.

--- src/tools/base/functions/firebase/fireInit.ts
+++ src/tools/base/functions/firebase/fireInit.ts
@@ -1,15 +1,16 @@
 
+import FB_APP from '@firebase/app';
+
 type Tprops = { args: any; pass: { fbConfig: any; arrFuncs: any[] } };
 
 export const fireInit = async (props: Tprops) => {
   // ---------- set Props
   const { args, pass } = props;
   const { fbConfig, arrFuncs } = pass;
-
+console.log({fbConfig});
   // ---------- set Caps Inputs
 
   // ---------- set Local Imports
-  const FB_APP = await import('@firebase/app');
 
   if (typeof fbConfig === 'object') {
     // ---------- set FB Init on a Variable
