3fdaed032c61d1abc541a2d66557d63001b42e90
Carlos A. <carlosvsk@gmail.com>
2024-09-13 11:37:17 -0300

    Commit generated by Flaxboll.

--- src/tools/base/functions/firebase/fireInit.ts
+++ src/tools/base/functions/firebase/fireInit.ts
@@ -1,8 +1,10 @@
 
 import * as FB_APP from 'firebase/app';
+
 type Tprops = { args: any; pass: { fbConfig: any; arrFuncs: any[] } };
 
 export const fireInit = async (props: Tprops) => {
+  console.log('dentro do FBINIT 1');
   // ---------- set Props
   const { args, pass } = props;
   const { fbConfig, arrFuncs } = pass;
@@ -16,7 +18,7 @@ export const fireInit = async (props: Tprops) => {
     // ---------- set FB Init on a Variable
     const fbInit = FB_APP.initializeApp(fbConfig, 'secondary');
 
-    console.log('dentro do FBINIT', { fbInit });
+    console.log('dentro do FBINIT 2', { fbInit });
 
     for (const currFunc of arrFuncs) {
       await currFunc(fbInit, args);
@@ -25,3 +27,4 @@ export const fireInit = async (props: Tprops) => {
     console.log('fbConfig precisa ser um objeto', { fbConfig });
   }
 };
+
